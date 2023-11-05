372f4a57d0ca64ea0ba0ecbc382c590956df64a7
Diahdwi09 <diahdwisulintangg@gmail.com>
2023-11-05 20:15:45 +0700

    lanjutan lagi

--- Laptop.php
+++ Laptop.php
@@ -1,12 +1,12 @@
-<?php
+dc <?php
 include_once "Device.php";
 
 class Laptop implements Device{
-    public function lihat_RAM(): void{
-        echo "8 GB";
+    public function namaMerk(): void{
+        echo "Asus";
     }
 
-    public function lihat_Harga(): void{
+    public function harga(): void{
         echo "6.000.000";
     }
 }
